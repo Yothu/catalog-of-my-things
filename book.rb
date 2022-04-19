8154bc65c01d8557ecb39bcfc17630e917fd3a5a
Reem <janinareem@gmail.com>
2022-04-19 17:21:02 +0200

    Update book method

--- book.rb
+++ book.rb
@@ -3,14 +3,14 @@ require_relative './item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publish_date, archived, id, publisher, cover_state)
+  def initialize(publish_date, archived, publisher, cover_state, id = Time.now.to_f.to_s)
     super(publish_date, archived, id)
     @publisher = publisher
     @cover_state = cover_state
   end
 
   def can_be_archived?
-    return true if can_be_archived? == true || @cover_state == 'bad'
+    return true if super == true || @cover_state == 'bad'
 
     false
   end
