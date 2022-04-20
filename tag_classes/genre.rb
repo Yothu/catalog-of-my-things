c1e449ebc5e8b53e43eea7edfd859fe37f02cf46
Reem <janinareem@gmail.com>
2022-04-20 21:25:41 +0200

    Fix linters

--- tag_classes/genre.rb
+++ tag_classes/genre.rb
@@ -9,7 +9,9 @@ class Genre
   end
 
   def add_item(item)
-    items << item unless items.include?(item)
-    item.genre = self
+    items << item
+    item.genre = self unless item.genre == self
   end
+
+  private :id, :items
 end
