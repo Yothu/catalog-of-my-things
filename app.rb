e7bf99f26d7b37bcd95410b433ff52372d5d4aba
Reem <janinareem@gmail.com>
2022-04-20 20:27:13 +0200

    Update paths for files

--- app.rb
+++ app.rb
@@ -1,37 +1,46 @@
-require_relative './book'
+require_relative './itemClasses/book'
+require_relative './listBooks'
+require_relative './listLabels'
+require_relative './preserveBook'
+require_relative './preserveLabel.rb'
+require 'json'
 
 class App
-  def initialize
-    @books = []
-    @labels = []
+include preserveBook
+include preserveLabel
+
+ book_list = ListBooks.new
+ label_list = ListLabel.new
+
+def options
+  input = gets.chomp.to_i
+  case input
+  when 1 then book_list.list_all_books
+  when 2 then list_all_music_albums
+  when 3 then list_all_games
+  when 4 then list_all_movies
+  when 5 then list_all_genre
+  when 6 then  label_list.list_all_labels
+  when 7 then list_all_authors
+  when 8 then list_all_sources
+  when 9 then book_list.add_book
+  when 10 then add_music_album
+  when 11 then add_game
+  when 12 then add_movie
+  when 13 then
+    save_data
+    13
+  else
+    puts 'Invalid number, please try again!'
   end
+end
 
-  def list_all_books
-    @books.each do |book|
-      puts "publish_date: #{book.publish_date}, archived: #{book.archived},
-      publisher : #{book.publisher}, cover_state: #{book.cover_state}"
-    end
-  end
+def save_data
+  save_books(books)
 
-  def list_all_labels
-    @labels.each do |label|
-      puts "title: #{label.title}, color: #{label.color}"
-    end
-  end
+  save_labels(labels)
+end
 
-  def add_book
-    puts 'Please enter the publisher name?'
-    publisher = gets.chomp
-
-    puts 'Please enter the cover state of the book?'
-    cover_state = gets.chomp
-
-    puts 'Please enter "true" if the book is archived and "false" if it is not:[T or F]'
-    archived = gets.chomp
-
-    puts 'Please enter publish date?'
-    publish_date = gets.chomp
-
-    @books.push(Book.new(publish_date, archived, publisher, cover_state))
-  end
+loaded_books
+load_labels
 end
--- main.rb
+++ main.rb
@@ -17,27 +17,6 @@ def main_menu
   puts '13) Exit the program'
 end
 
-def options
-  input = gets.chomp.to_i
-  case input
-  when 1 then list_all_books
-  when 2 then list_all_music_albums
-  when 3 then list_all_games
-  when 4 then list_all_movies
-  when 5 then list_all_genre
-  when 6 then list_all_labels
-  when 7 then list_all_authors
-  when 8 then list_all_sources
-  when 9 then add_book
-  when 10 then add_music_album
-  when 11 then add_game
-  when 12 then add_movie
-  when 13 then 13
-  else
-    puts 'Invalid number, please try again!'
-  end
-end
-
 def main
   running = true
   app = App.new
