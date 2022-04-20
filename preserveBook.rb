8d01bf9b6483a300f8cfba99ade1478d882c047e
Reem <janinareem@gmail.com>
2022-04-20 20:26:17 +0200

    Add files to preserve books daat into json

---
+++ preserveBook.rb
@@ -0,0 +1,33 @@
+require_relative './itemClasses/book'
+
+module BookJson
+
+def save_books(books)
+  books = @books.each_with_index.map do |book, index|
+    {
+      publish_date: book.publish_date, archived: book.archived,
+       publisher: book.publisher, cover_state: book.cover_state, index: index
+    }
+  end
+    File.write('books.json', JSON.generate(books))
+end
+
+def load_books
+  return [] unless File.exist?('books.json')
+
+    file = File.open('books.json')
+    read_file = File.read(file)
+    read_json = JSON.parse(read_file)
+
+    loaded_books = []
+
+    loaded_books.each do |book|
+      load_books.push(Book.new(book['publish_date'], book['archived'],
+       book['publisher'], book[' cover_state']))
+    end
+  
+    loaded_books
+    file.close
+end
+
+end
