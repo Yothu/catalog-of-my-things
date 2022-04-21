8260424ae0e4dd6dd107f67b714e87492b6a6921
David Vergaray <david.vergaray.a@gmail.com>
2022-04-21 14:19:14 -0400

    Add MusicAlbum and Genre save and load data methods to main

--- app.rb
+++ app.rb
@@ -2,15 +2,19 @@ require_relative './list_books'
 require_relative './list_label'
 require_relative './game_list'
 require_relative './author_list'
+require_relative './genre_list'
+require_relative './music_album_list'
 
 class App
-  attr_accessor :books, :games, :labels, :authors
+  attr_accessor :books, :games, :labels, :authors, :genres, :musics
 
   def initialize
     @books = ListBooks.new
     @games = GameList.new
     @labels = ListLabel.new
     @authors = AuthorList.new
+    @genres = GenreList.new
+    @musics = MusicList.new
   end
 
   def obtain_data
@@ -18,6 +22,8 @@ class App
     @games.obtain_games
     @labels.load_labels
     @authors.obtain_authors
+    @genres.display_genres
+    @musics.show_albums
   end
 
   def save_data
@@ -25,5 +31,7 @@ class App
     @games.save_games
     @labels.save_labels
     @authors.save_authors
+    @genres.save_genres
+    @musics.save_albums
   end
 end
--- main.rb
+++ main.rb
@@ -21,15 +21,15 @@ def options(app)
   input = gets.chomp.to_i
   case input
   when 1 then app.books.list_all_books
-  when 2 then app.list_all_music_albums
+  when 2 then app.musics.list_all_music_albums
   when 3 then app.games.list_all_games
   when 4 then app.list_all_movies
-  when 5 then app.list_all_genre
+  when 5 then app.genres.list_all_genre
   when 6 then app.labels.list_all_labels
   when 7 then app.authors.list_all_authors
   when 8 then app.list_all_sources
   when 9 then app.books.add_book
-  when 10 then app.add_music_album
+  when 10 then app.musics.add_music_album
   when 11 then app.games.add_game_menu
   when 12 then app.add_movie
   when 13 then 13
